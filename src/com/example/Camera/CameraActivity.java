d3a8c3c306962149c3c0c03c91a9d9a60d275ff3
Xvos <septor@tut.by>
2015-06-14 23:17:33 +0300

    Start
    First

---
+++ src/com/example/Camera/CameraActivity.java
@@ -0,0 +1,236 @@
+package com.example.Camera;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.view.ViewGroup.LayoutParams;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.opencv.android.*;
+
+public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2
+{
+    private static final java.lang.String TAG = "OpenCVLoader/BaseLoaderCallback";
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+    private SurfaceView preview;
+    private Button shotBtn;
+    private JavaCameraView cvCameraView;
+
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this)
+    {
+        @Override
+        public void onManagerConnected(int status)
+        {
+            switch (status)
+            {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV Loaded");
+                    cvCameraView.enableView();
+                    break;
+                }
+                default:
+                {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+       /* cvCameraView = (JavaCameraView) findViewById(R.id.CameraActivityView);
+        cvCameraView.setVisibility(SurfaceView.VISIBLE);
+        cvCameraView.setCvCameraViewListener(this);*/
+
+        // наше SurfaceView имеет имя SurfaceView01
+        preview = (SurfaceView) findViewById(R.id.SurfaceView01);
+
+        surfaceHolder = preview.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+       // shotBtn = (Button) findViewById(R.id.Button01);
+        //shotBtn.setText("Shot");
+       // shotBtn.setX((getWindow().getDecorView().getWidth() - shotBtn.getWidth()) / 2);
+
+       // shotBtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_7, this, loaderCallback);
+        //camera = Camera.open();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+       /* if (camera != null)
+        {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }*/
+    }
+
+   /* @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        try
+        {
+            camera.setPreviewDisplay(holder);
+            camera.setPreviewCallback(this);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        float aspect = (float) previewSize.width / previewSize.height;
+
+        int previewSurfaceWidth = preview.getWidth();
+        int previewSurfaceHeight = preview.getHeight();
+
+        LayoutParams lp = preview.getLayoutParams();
+
+        // здесь корректируем размер отображаемого preview, чтобы не было искажений
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+        {
+            // портретный вид
+            camera.setDisplayOrientation(90);
+            lp.height = previewSurfaceHeight;
+            lp.width = (int) (previewSurfaceHeight / aspect);
+            ;
+        }
+        else
+        {
+            // ландшафтный
+            camera.setDisplayOrientation(0);
+            lp.width = previewSurfaceWidth;
+            lp.height = (int) (previewSurfaceWidth / aspect);
+        }
+
+        preview.setLayoutParams(lp);
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v == shotBtn)
+        {
+            // либо делаем снимок непосредственно здесь
+            // либо включаем обработчик автофокуса
+
+            camera.takePicture(null,  null, null, this);
+            // camera.autoFocus(this);
+        }
+    }
+
+    @Override
+    public void onPictureTaken(byte[] paramArrayOfByte, Camera paramCamera)
+    {
+        // сохраняем полученные jpg в папке /sdcard/CameraExample/
+        // имя файла - System.currentTimeMillis()
+
+        try
+        {
+            File saveDir = new File("/sdcard/CameraExample/");
+
+            if (!saveDir.exists())
+            {
+                saveDir.mkdirs();
+            }
+
+            FileOutputStream os = new FileOutputStream(String.format("/sdcard/CameraExample/%d.jpg", System.currentTimeMillis()));
+            os.write(paramArrayOfByte);
+            os.close();
+        }
+        catch (Exception e)
+        {
+        }
+
+
+
+        // после того, как снимок сделан, показ превью отключается. необходимо включить его
+        paramCamera.startPreview();
+    }
+
+    @Override
+    public void onAutoFocus(boolean paramBoolean, Camera paramCamera)
+    {
+        //if (paramBoolean)
+        //{
+            // если удалось сфокусироваться, делаем снимок
+            //paramCamera.takePicture(null, null, null, this);
+        //}
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] paramArrayOfByte, Camera paramCamera)
+    {
+        // здесь можно обрабатывать изображение, показываемое в preview
+        Bitmap bmp = BitmapFactory.decodeByteArray(paramArrayOfByte, 0, paramArrayOfByte.length);
+    }*/
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if(cvCameraView != null)
+        {
+            cvCameraView.disableView();
+        }
+    }
+
+    public void onCameraViewStarted(int width, int height)
+    { /* compiled code */ }
+
+    public void onCameraViewStopped()
+    { /* compiled code */ }
+
+    public org.opencv.core.Mat onCameraFrame(org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame inputFrame)
+    {
+        return inputFrame.rgba();
+    }
+}
