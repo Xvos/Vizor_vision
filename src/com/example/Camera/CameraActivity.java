5321e2460038588174cc450994282252d6ba20ae
Xvos <septor@tut.by>
2015-06-15 23:00:36 +0300

    Отключил OpenCV

--- src/com/example/Camera/CameraActivity.java
+++ src/com/example/Camera/CameraActivity.java
@@ -9,9 +9,7 @@ import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
 import android.view.ViewGroup.LayoutParams;
 
@@ -21,49 +19,29 @@ import java.io.IOException;
 
 import org.opencv.android.*;
 
-public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2
+public class CameraActivity extends Activity implements SurfaceHolder.Callback, View.OnClickListener, Camera.PictureCallback, Camera.PreviewCallback, Camera.AutoFocusCallback,
+        Camera.FaceDetectionListener
 {
-    private static final java.lang.String TAG = "OpenCVLoader/BaseLoaderCallback";
-
     private Camera camera;
     private SurfaceHolder surfaceHolder;
     private SurfaceView preview;
     private Button shotBtn;
-    private JavaCameraView cvCameraView;
 
-    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this)
-    {
-        @Override
-        public void onManagerConnected(int status)
-        {
-            switch (status)
-            {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV Loaded");
-                    cvCameraView.enableView();
-                    break;
-                }
-                default:
-                {
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-        }
-    };
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-       /* cvCameraView = (JavaCameraView) findViewById(R.id.CameraActivityView);
-        cvCameraView.setVisibility(SurfaceView.VISIBLE);
-        cvCameraView.setCvCameraViewListener(this);*/
+        // если хотим, чтобы приложение постоянно имело портретную ориентацию
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // если хотим, чтобы приложение было полноэкранным
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // и без заголовка
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.main);
 
         // наше SurfaceView имеет имя SurfaceView01
         preview = (SurfaceView) findViewById(R.id.SurfaceView01);
@@ -72,20 +50,17 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-       // shotBtn = (Button) findViewById(R.id.Button01);
-        //shotBtn.setText("Shot");
-       // shotBtn.setX((getWindow().getDecorView().getWidth() - shotBtn.getWidth()) / 2);
-
-       // shotBtn.setOnClickListener(this);
+        // кнопка имеет имя Button01
+        shotBtn = (Button) findViewById(R.id.Button01);
+        shotBtn.setText("Shot");
+        shotBtn.setOnClickListener(this);
     }
-
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_7, this, loaderCallback);
-        //camera = Camera.open();
+        camera = Camera.open();
     }
 
     @Override
@@ -93,16 +68,25 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
     {
         super.onPause();
 
-       /* if (camera != null)
+        if (camera != null)
         {
             camera.setPreviewCallback(null);
             camera.stopPreview();
             camera.release();
             camera = null;
-        }*/
+        }
     }
 
-   /* @Override
+
+    @Override
+    public void onFaceDetection(Camera.Face[] faces, Camera camera)
+    {
+
+    }
+
+
+
+        @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
     {
     }
@@ -161,10 +145,10 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         if (v == shotBtn)
         {
             // либо делаем снимок непосредственно здесь
-            // либо включаем обработчик автофокуса
+            // 	либо включаем обработчик автофокуса
 
-            camera.takePicture(null,  null, null, this);
-            // camera.autoFocus(this);
+            //camera.takePicture(null, null, null, this);
+            camera.autoFocus(this);
         }
     }
 
@@ -191,8 +175,6 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         {
         }
 
-
-
         // после того, как снимок сделан, показ превью отключается. необходимо включить его
         paramCamera.startPreview();
     }
@@ -200,37 +182,16 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
     @Override
     public void onAutoFocus(boolean paramBoolean, Camera paramCamera)
     {
-        //if (paramBoolean)
-        //{
+        if (paramBoolean)
+        {
             // если удалось сфокусироваться, делаем снимок
-            //paramCamera.takePicture(null, null, null, this);
-        //}
+            paramCamera.takePicture(null, null, null, this);
+        }
     }
 
     @Override
     public void onPreviewFrame(byte[] paramArrayOfByte, Camera paramCamera)
     {
         // здесь можно обрабатывать изображение, показываемое в preview
-        Bitmap bmp = BitmapFactory.decodeByteArray(paramArrayOfByte, 0, paramArrayOfByte.length);
-    }*/
-
-    public void onDestroy()
-    {
-        super.onDestroy();
-        if(cvCameraView != null)
-        {
-            cvCameraView.disableView();
-        }
-    }
-
-    public void onCameraViewStarted(int width, int height)
-    { /* compiled code */ }
-
-    public void onCameraViewStopped()
-    { /* compiled code */ }
-
-    public org.opencv.core.Mat onCameraFrame(org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame inputFrame)
-    {
-        return inputFrame.rgba();
     }
 }
