55d519d99b20d3878206c3d53715b0dc467e056c
netherwire <r.chehowsky@gmail.com>
2015-07-02 00:49:26 +0300

    Добавил код фильтров

--- src/com/example/Camera/editor/filter/CropFilter.java
+++ src/com/example/Camera/editor/filter/CropFilter.java
@@ -1,7 +1,56 @@
 package com.example.Camera.editor.filter;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by netherwire on 7/1/15.
  */
-public class CropFilter {
+public class CropFilter implements Filter {
+
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
+
+    // X and y coordinates on an output bitmap
+    private final int destX;
+    private final int destY;
+
+    public CropFilter(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.destX = 0;
+        this.destY = 0;
+    }
+
+    public CropFilter(int x, int y, int width, int height, int destX, int destY) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.destX = destX;
+        this.destY = destY;
+    }
+
+    @Override
+    public void process(Bitmap source, Bitmap destination) {
+
+        int destWidth = destination.getWidth();
+        int destHeight = destination.getHeight();
+
+        int shiftWidth = Math.min(source.getWidth() - x, Math.min(destination.getWidth(), width));
+        int shiftHeight = Math.min(source.getHeight() - y, Math.min(destination.getHeight(), height));
+
+        int[] pixels = new int[shiftWidth * shiftHeight];
+        source.getPixels(pixels, 0, shiftWidth, x, y, shiftWidth, shiftHeight);
+
+        shiftWidth = Math.min(shiftWidth, destination.getWidth() - destX);
+        shiftHeight = Math.min(shiftHeight, destination.getHeight() - destY);
+
+        destination.setPixels(pixels, 0, shiftWidth, destX, destY, shiftWidth, shiftHeight);
+    }
 }
