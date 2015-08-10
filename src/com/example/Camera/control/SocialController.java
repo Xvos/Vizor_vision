714617ba40ea0f4586f771cc24ffbe5fb25327e8
Fedor <uran230@gmail.com>
2015-08-10 22:32:35 +0300

    Telegram and Instagram posting

--- src/com/example/Camera/control/SocialController.java
+++ src/com/example/Camera/control/SocialController.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -23,17 +24,28 @@ public class SocialController {
     }
 
     public static void postOnInstagram(Activity activity, byte[] imageByteArray, String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/jpeg");
+        intent.setPackage("com.instagram.android");
+        if (intent != null)
+        {
+            intent.putExtra(Intent.EXTRA_STREAM, SaveController.pictureUri);
+            activity.startActivity(Intent.createChooser(intent, "Share with"));
+        }
+        else
+        {
+            Toast.makeText(activity.getApplicationContext(), "Instagram is not installed", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static void postOnTelegram(Activity activity, byte[] imageByteArray, String text)
     {
         Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
+        intent.setType("image/jpeg");
         intent.setPackage("org.telegram.messenger");
         if (intent != null)
         {
-            intent.putExtra(Intent.EXTRA_TEXT, text);
-            intent.putExtra(Intent.EXTRA_STREAM, SaveController.lastSavedPicture);
+            intent.putExtra(Intent.EXTRA_STREAM, SaveController.pictureUri);
             activity.startActivity(Intent.createChooser(intent, "Share with"));
         }
         else
